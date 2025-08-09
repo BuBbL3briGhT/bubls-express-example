0eec046d4a3e3ae0497672d207daf8fb1a5a3927
BaMBi <bAmBi@baCKdOor.niL>
2025-08-09 19:28:46 -0400

    Delegate configuration 🦤

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const BubbleScript = require("bubblescript");
-const path = require("path");
-const fs = require("fs");
-
-const { mkfn } = BubbleScript;
-
-// console.log(BubbleScript);
-
-// BubbleScript.rootBinding.require = require;
-BubbleScript.rootBinding.require =
-  mkfn(o => require(...o));
-
-let loadFile = function (filePath) {
-  filePath = path.join(__dirname, filePath);
-  return BubbleScript.eval(
-    fs.readFileSync(filePath, 'utf-8'));
-}
-
-// BubbleScript.loadFile("./index.bubls");
-loadFile("./index.bubls");
-// loadFile("./hi.bubls");
-
-// console.log(BubbleScript);
+const bubls = require("bubblescript");
+bubls.configure({ require, __dirname });
+bubls.load("./index.bubls");
